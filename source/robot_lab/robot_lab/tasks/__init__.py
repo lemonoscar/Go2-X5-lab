7b89f02ced8ea522a0a32d7707552eb1eed374c2
lemonoscar <3467057703@qq.com>
2026-04-11 09:49:35 -0700

    fix: add vla_sft to blacklist due to version compatibility

--- source/robot_lab/robot_lab/tasks/__init__.py
+++ source/robot_lab/robot_lab/tasks/__init__.py
@@ -19,6 +19,6 @@ from isaaclab_tasks.utils import import_packages
 
 
 # The blacklist is used to prevent importing configs from sub-packages
-_BLACKLIST_PKGS = ["utils"]
+_BLACKLIST_PKGS = ["utils", "vla_sft"]  # vla_sft 有版本兼容问题，暂时跳过
 # Import all configs in this package
 import_packages(__name__, _BLACKLIST_PKGS)
